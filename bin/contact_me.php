d7d53e346bb244f006960d0a8b40b0af78d36608
fvaldes <fernandoluis.valdes@bbva.com>
2017-07-28 13:46:05 +0000

    mail

--- bin/contact_me.php
+++ bin/contact_me.php
@@ -30,12 +30,13 @@ $email_body = "Detalles:\n\nNombre: $name\n\nEmail: $email_address\n\nTelefono: 
 
 $mail = new PHPMailer;
 $mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->SMTPDebug = 1;
 $mail->Host = 'smtp.gmail.com;';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'fervaldes11@gmail.com';                 // SMTP username
 $mail->Password = 'Ktufernando01';                           // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
 
 $mail->setFrom('arteyregalostore@gmail.com', 'Mailer');
 $mail->addAddress('arteyregalostore@gmail.com');     // Add a recipient
